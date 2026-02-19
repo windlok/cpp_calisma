6bc6339c13429d07035e3cbb7721df403be1149b
User <User@DESKTOP-NMATV2J>
2026-02-19 09:15:18 +0300

    degisiklik_1

--- shred_ptr/shred_ptr.cpp
+++ shred_ptr/shred_ptr.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+class Silah {
+public:
+	int d{};
 
+    Silah() {
+		std::cout << "Silah olusturuldu." << std::endl;
+    }
+	~Silah() {
+		std::cout << "Silah yok edildi." << std::endl;
+	}
+};
 int main()
 {
-    std::cout << "Hello World!\n";
+	std::shared_ptr<Silah> ptr = std::make_shared<Silah>();
+	std::shared_ptr<Silah> ptr2 = ptr;
+
+	std::cout<< ptr->d << std::endl << "ptr kullaniliyor." << std::endl;
+	
+	std::cout << "ptr gercek adresi: " << ptr.get() << std::endl;
+	std::cout << "ptr referans sayisi: " << ptr.use_count() << std::endl;
 }
